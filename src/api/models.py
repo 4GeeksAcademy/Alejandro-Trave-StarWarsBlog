be1014d474675f57d79e33b4d1ff25913136cfce
Alejandro Travé <121689156+Atravep@users.noreply.github.com>
2024-06-03 19:11:13 +0000

    agrego modelos

--- src/api/models.py
+++ src/api/models.py
@@ -9,6 +9,8 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column(db.String(), unique=False, nullable=True)
 
     def __repr__(self):
         return f'<User: {self.email}>'
@@ -17,4 +19,144 @@ class Users(db.Model):
         # Do not serialize the password, its a security breach
         return {'id': self.id,
                 'email': self.email,
-                'is_active': self.is_active}
+                'is_active': self.is_active,
+                'first_name': self.first_name,
+                'last_name': self.last_name}
+    
+
+class Posts(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String())
+    body = db.Column(db.String(), nullable=False)
+    date_publication = db.Column(db.Date(), nullable=False)
+    image_url = db.Column(db.String())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id])
+
+    def __repr__(self):
+        return f'<Post: {self.title}>'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'title': self.title,
+                'description': self.description,
+                'body': self.body,
+                'date_publication': self.date_publication,
+                'image_url': self.image_url,
+                'user_id': self.user_id}
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    body = db.Column(db.String(), nullable=False)
+    date_publication = db.Column(db.Date(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id])
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    user_to = db.relationship('Posts', foreign_keys=[post_id])
+
+    def __repr__(self):
+        return f'<Post: {self.title}>'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'title': self.title,
+                'body': self.body,
+                'date_publication': self.date_publication,
+                'image_url': self.image_url,
+                'user_id': self.user_id,
+                'post_id': self.post_id}
+
+
+class Characters(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    last_name = db.Column(db.String(), nullable=False)
+    height = db.Column(db.Float(), nullable=True)
+    mass = db.Column(db.Float(), nullable=True)
+    hair_color = db.Column(db.String(), nullable=True)
+    skin_color = db.Column(db.String(), nullable=True)
+    eye_color = db.Column(db.String(), nullable=True)
+    birth_year = db.Column(db.Integer(), nullable=True)
+    gender = db.Column(db.String(), nullable=True)
+    home_world = db.Column(db.String(), nullable=True)
+    planet_id = db.Column(db.Integer(), db.ForeignKey('planets.id'))
+    planet_to = db.relationship('Planets', foreign_keys=[planet_id])
+    def __repr__(self):
+        return f'<Post: {self.name} {self.last_name}>'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'name': self.name,
+                'last_name': self.last_name,
+                'height' : self.height,
+                'mass' : self.mass,
+                'hair_color' : self.hair_color,
+                'skin_color' : self.skin_color,
+                'eye_color' : self.eye_color,
+                'birth_year' : self.birth_year,
+                'gender' : self.gender,
+                'home_world' : self.home_world,
+                'planet_id' : self.planet_id}
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    diameter = db.Column(db.Float(), nullable=True)
+    rotation_period = db.Column(db.Float(), nullable=True)
+    orbital_period = db.Column(db.Float(), nullable=True)
+    gravity = db.Column(db.String(), nullable=True)
+    population = db.Column(db.String(), nullable=True)
+    climate = db.Column(db.String(), nullable=True)
+    terrain = db.Column(db.String(), nullable=True)
+    surface_water = db.Column(db.Integer(), nullable=True)
+
+    def __repr__(self):
+        return f'<Post: {self.name}'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'name': self.name,
+                diameter : self.diameter,
+                rotation_period : self.rotation_period,
+                orbital_period : self.orbital_period,
+                gravity : self.gravity,
+                population : self.population,
+                climate : self.climate,
+                terrain : self.terrain,
+                surface_water : self.surface_water}
+
+class Species(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    classification = db.Column(db.Float(), nullable=True)
+    designation = db.Column(db.Float(), nullable=True)
+    language = db.Column(db.Float(), nullable=True)
+    avg_lifespan = db.Column(db.String(), nullable=True)
+    avg_height = db.Column(db.String(), nullable=True)
+    hair_colors = db.Column(db.String(), nullable=True)
+    skin_colors = db.Column(db.String(), nullable=True)
+    eye_colors = db.Column(db.Integer(), nullable=True)
+
+    def __repr__(self):
+        return f'<Post: {self.name}'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'name': self.name,
+                classification : self.classification,
+                designation : self.designation,
+                language : self.language,
+                avg_lifespan : self.avg_lifespan,
+                avg_height : self.avg_height,
+                hair_colors : self.hair_colors,
+                skin_colors : self.skin_colors,
+                eye_colors : self.eye_colors}
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
+from api.models import db, Users, Posts, Characters, Planets, Species
 
 
 api = Blueprint('api', __name__)
@@ -16,3 +19,195 @@ def handle_hello():
     response_body = {}
     response_body['message'] = "Hello! I'm a message that came from the backend"
     return response_body, 200
+
+@api.route("/login", methods=["POST"])
+def login():
+    response_body={}
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = db.session.execute(db.select(Users).where(Users.email==email, Users.password == password, Users.is_active == True)).scalar()
+    if user:
+        access_token = create_access_token(identity={'user_id': user.id})
+        response_body['message'] = 'User Logeado'
+        response_body['access_token'] = access_token
+        return response_body, 200
+    response_body['message'] = 'Bad username or password'
+    return response_body, 401
+
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/profile", methods=["GET"])
+@jwt_required()
+def profile():
+    response_body = {}
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    print(current_user)
+    response_body['message'] = f'User logueado: {current_user}'
+    return response_body, 200
+
+
+@api.route('/users', methods=['GET', 'POST'])
+def handle_user():
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Users)).scalars()
+        results = [row.serialize() for row in rows]
+        response_body['results'] = results
+        response_body['message'] = 'Listado de Usuarios'
+        return response_body, 200
+    if request.method == 'POST':
+        response_body['message'] = 'Haga login para modificar usuarios'
+        return response_body, 200
+
+@api.route('/posts', methods=['GET', 'POST'])
+@jwt_required()
+def handle_post():
+    response_body = {}
+    current_user = get_jwt_identity()
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Posts)).scalars()
+        results = [row.serialize() for row in rows]
+        response_body['results'] = results
+        response_body['message'] = 'Listado de Posts'
+        return response_body, 200
+    if request.method == 'POST':
+        response_body['message'] = 'Haga login para añadir un post'
+        return response_body, 200
+
+@api.route('/characters', methods=['GET', 'POST'])
+def handle_character():
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Characters)).scalars()
+        results = [row.serialize() for row in rows]
+        response_body['results'] = results
+        response_body['message'] = 'Listado de Personajes'
+        return response_body, 200
+    if request.method == 'POST':
+        response_body['message'] = 'Haga login para añadir un personaje'
+        return response_body, 200
+
+
+@api.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_users(user_id):
+    response_body = {}
+    if request.method == 'GET':
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if user:
+            response_body['results'] = user.serialize()
+            response_body['message'] = 'Usuario encontrado'
+            return response_body, 200
+        response_body['message'] = 'Usario inexistente'
+        response_body['results'] = {}
+        return response_body, 404
+    if request.method == 'PUT':
+        data = request.json
+        # TODO: Validación de datos recibidos 
+        print(data)
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if user:
+            user.email = data['email']
+            user.is_active = data['is_active']
+            user.last_name = data['last_name']
+            user.first_name = data['first_name']
+            db.session.commit()
+            response_body['message'] = 'Datos del usuario actualizados'
+            response_body['results'] = user.serialize()
+            return response_body, 200
+        response_body['message'] = 'Usario inexistente'
+        response_body['results'] = {}
+        return response_body, 404
+    if request.method == 'DELETE':
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if user:
+            # db.session.delete(user)
+            user.is_active = False
+            db.session.commit()
+            response_body['message'] = 'Usuario eliminado'
+            response_body['results'] = {}
+        response_body['message'] = 'Usuario inexistente'
+        response_body['results'] = {}
+        return response_body, 200
+
+@api.route('/posts/<int:post_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_posts(posts_id):
+    response_body = {}
+    if request.method == 'GET':
+        post = db.session.execute(db.select(Posts).where(Posts.id == post_id)).scalar()
+        if post:
+            response_body['results'] = post.serialize()
+            response_body['message'] = 'Post encontrado'
+            return response_body, 200
+        response_body['message'] = 'Post inexistente'
+        response_body['results'] = {}
+        return response_body, 404
+    if request.method == 'PUT':
+        data = request.json
+        # TODO: Validación de datos recibidos 
+        print(data)
+        post = db.session.execute(db.select(Posts).where(Posts.id == post_id)).scalar()
+        if post:
+            post.description = data['description']
+            post.body = data['body']
+            post.image_url = data['image_url']
+            db.session.commit()
+            response_body['message'] = 'Post editado'
+            response_body['results'] = post.serialize()
+            return response_body, 200
+        response_body['message'] = 'Post inexistente'
+        response_body['results'] = {}
+        return response_body, 404
+    if request.method == 'DELETE':
+        post = db.session.execute(db.select(Posts).where(Posts.id == posts_id)).scalar()
+        if post:
+            # db.session.delete(post)
+            post.is_active = False
+            db.session.commit()
+            response_body['message'] = 'Post eliminado'
+            response_body['results'] = {}
+        response_body['message'] = 'Post inexistente'
+        response_body['results'] = {}
+        return response_body, 200
+
+@api.route('/characters/<int:character_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_characters(character_id):
+    response_body = {}
+    if request.method == 'GET':
+        character = db.session.execute(db.select(Characters).where(Character.id == character_id)).scalar()
+        if character:
+            response_body['results'] = character.serialize()
+            response_body['message'] = 'Personaje encontrado'
+            return response_body, 200
+        response_body['message'] = 'Personaje inexistente'
+        response_body['results'] = {}
+        return response_body, 404
+    if request.method == 'PUT':
+        data = request.json
+        # TODO: Validación de datos recibidos 
+        print(data)
+        character = db.session.execute(db.select(Characters).where(Characters.id == character_id)).scalar()
+        if character:
+            character.description = data['description']
+            character.body = data['body']
+            character.image_url = data['image_url']
+            db.session.commit()
+            response_body['message'] = 'Personaje editado'
+            response_body['results'] = character.serialize()
+            return response_body, 200
+        response_body['message'] = 'Personaje inexistente'
+        response_body['results'] = {}
+        return response_body, 404
+    if request.method == 'DELETE':
+        character = db.session.execute(db.select(Characters).where(Characters.id == character_id)).scalar()
+        if character:
+            # db.session.delete(character)
+            character.is_active = False
+            db.session.commit()
+            response_body['message'] = 'Personaje eliminado'
+            response_body['results'] = {}
+        response_body['message'] = 'Personaje inexistente'
+        response_body['results'] = {}
+        return response_body, 200
